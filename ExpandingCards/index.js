a99fbd7fb4323613c7ec532d8896558cb6f4ea10
Soukarsha Dhar <soukarshadhar91@gmail.com>
2024-10-08 12:32:47 +0530

    first commit of component

---
+++ ExpandingCards/index.js
@@ -0,0 +1,24 @@
+const ROOT_ID = "root";
+const rootElement = document.getElementById(ROOT_ID);
+const cards = Array.from(rootElement.children);
+
+const handleOnRootClick = (event) => {
+  console.log("1");
+
+  cards.forEach((card) => {
+    const isCardExpanded = card.classList.contains("expand");
+    if (event.target.id === ROOT_ID && isCardExpanded) {
+      card.classList.remove("expand");
+    }
+
+    if (event.target === card && !isCardExpanded) {
+      card.classList.add("expand");
+    }
+
+    if (event.target !== card && isCardExpanded) {
+      card.classList.remove("expand");
+    }
+  });
+};
+
+rootElement.addEventListener("click", handleOnRootClick);
